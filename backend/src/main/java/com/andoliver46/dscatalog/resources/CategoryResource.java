55e5868b315cef9899a98d4f1b99caa3a198d4f0
AndOliver46 <anderson_souza75@live.com>
2023-01-20 21:24:42 -0300

    Find Category By id, exception handling

--- backend/src/main/java/com/andoliver46/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/andoliver46/dscatalog/resources/CategoryResource.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andoliver46.dscatalog.dto.CategoryDTO;
+import com.andoliver46.dscatalog.entities.Category;
 import com.andoliver46.dscatalog.services.CategoryService;
 
 @RestController
@@ -22,6 +24,12 @@ public class CategoryResource {
 	public ResponseEntity<List<CategoryDTO>> findAll(){
 		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+		CategoryDTO cat = service.findById(id);
+		return ResponseEntity.ok(cat);
 	}
 	
 	
--- backend/src/main/java/com/andoliver46/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/andoliver46/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.andoliver46.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,17 +10,27 @@ import org.springframework.transaction.annotation.Transactional;
 import com.andoliver46.dscatalog.dto.CategoryDTO;
 import com.andoliver46.dscatalog.entities.Category;
 import com.andoliver46.dscatalog.repositories.CategoryRepository;
+import com.andoliver46.dscatalog.services.exceptions.EntityNotFoundException;
+
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repo;
 
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
+	public List<CategoryDTO> findAll() {
 		List<Category> list = repo.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).toList();
 	}
-	
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repo.findById(id);
+		Category catDTO = obj
+				.orElseThrow(() -> new EntityNotFoundException("A categoria de Id: " + id + " não foi encontrada."));
+		return new CategoryDTO(catDTO);
+	}
+
 }
