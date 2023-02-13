c1d4f19d3dfb70bd8076cdd2a21bb40e677857c0
AndOliver46 <anderson_souza75@live.com>
2023-02-13 15:48:01 -0300

    Feat: Cors, basic  implementation

--- backend/src/main/java/com/andoliver46/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/andoliver46/dscatalog/config/ResourceServerConfig.java
@@ -3,7 +3,10 @@ package com.andoliver46.dscatalog.config;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +14,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -18,14 +25,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private JwtTokenStore tokenStore;
 
 	private static final String[] LOGIN = { "/oauth/token", };
 	private static final String[] PRODUCTS_AND_CATEGORIES = { "/products/**", "/categories/**" };
 	private static final String[] USERS = { "/users/**" };
-	private static final String[] H2 = {"/h2-console/**"};
+	private static final String[] H2 = { "/h2-console/**" };
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -34,19 +41,41 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		
-		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
-		
-		http.authorizeRequests()
+
+		http
+			.authorizeRequests()
 			.antMatchers(LOGIN).permitAll()
 			.antMatchers(H2).permitAll()
 			.antMatchers(HttpMethod.GET, PRODUCTS_AND_CATEGORIES).permitAll()
 			.antMatchers(PRODUCTS_AND_CATEGORIES).hasAnyRole("OPERATOR", "ADMIN")
 			.antMatchers(USERS).hasRole("ADMIN")
 			.anyRequest().authenticated();
+
+		http.cors().configurationSource(corsConfigurationSource());
 			
-			
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOriginPatterns(Arrays.asList("*"));
+		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		return source;
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
+				new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 }
